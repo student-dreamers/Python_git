abbb2b99a4c80a29550c8bb99de901529d67ac95
David Žahour <davidzah6@seznam.cz>
2019-05-26 00:49:45 +0200

    ver přidat for

--- connect_data.py
+++ connect_data.py
@@ -8,7 +8,6 @@ def parse_ingedience(ingredience):
     lst = ingredience.split(",")
     list(map(str.strip, lst))
     lst = strip_list(lst)
-    print(lst)
     return lst 
 
 with open('conceptdata.json', encoding='utf-8') as json_file:
@@ -19,5 +18,15 @@ with open('conceptdata.json', encoding='utf-8') as json_file:
     product_description = data['ConceptsCollection'][0]['ProductDescription']
     categori_id = data['ConceptsCollection'][0]['CategoryIds'][0]
     imageurl = data['ConceptsCollection'][0]['ImageUrl']
+    concept_code = data ['ConceptsCollection'][0]['ConceptCode']
+    shop_url = "https://in.oriflame.com/products/product?code="+concept_code
+   # print(ingredienc ,original_name ,product_description,categori_id,imageurl,shop_url )
+    json_file.close()
 
-    print(ingredienc ,original_name ,product_description,categori_id,imageurl )
+with open('prices.json', encoding='utf-8') as price_json_file:
+    data = json.load(price_json_file)
+    currnency = data['ConceptPricesCollection'][0]['Currency']
+    price = data ['ConceptPricesCollection'][0]['Price']['CurrentPrice']
+    concept_code = data['ConceptPricesCollection'][0]['ConceptCode']
+    print(concept_code)
+
