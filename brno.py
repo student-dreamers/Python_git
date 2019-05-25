065d0efd0a39fb76e4f6dbcad540e3b51c4f5890
Barča Vobořilová <46422600+voborilovab@users.noreply.github.com>
2019-05-25 23:44:26 +0200

    Část funkce na insert data

--- brno.py
+++ brno.py
@@ -4,12 +4,61 @@ mydb = mysql.connector.connect(
   host="138.197.181.210",
   user="student_dreamers",
   passwd="1bJSe9hcO5Jkj2G2Klz7",
-   database="student_dreamers"
+  database="student_dreamers"
 )
-
 
 mycursor = mydb.cursor()
 
+def insert_data(data):
+    sql = """START TRANSACTION;
+DELETE FROM ingredient_influence;
+DELETE FROM data_product_ingredient;
+DELETE FROM influence;
+DELETE FROM data_product;
+DELETE FROM data_category;
+
+INSERT INTO `data_category` (`id`, `uuid`, `name`, `url_icon`) VALUES
+(1,	'test',	'test',	NULL),
+(2,	'test2',	'test2',	NULL);
+
+
+INSERT INTO `data_product` (`id`, `uuid`, `ean`, `category_id`, `name`, `description`, `url_image`, `url_shop`, `price`) VALUES
+(1,	'test',	'test',	1,	'test',	'',	'',	'',	4.00),
+(2,	'test2',	'test2',	1,	'test2',	'',	'',	'',	2.00);
+
+INSERT INTO `influence` (`id`, `name`, `description`) VALUES
+(1,	'Ničí to zvířátka',	'helpppp'),
+(2,	'Ničí to stromečky',	'pomoooooooc');
+
+INSERT INTO `data_product_ingredient` (`product_id`, `ingredient_name`, `order`, `amount`) VALUES
+(1,	'SUGAR',	0,	0.40000),
+(1,	'SALT',	1,	0.60000),
+(2,	'SALT',	0,	1);
+
+INSERT INTO `ingredient_influence` (`ingredient_name`, `influence_id`, `score`) VALUES
+('SUGAR',	1,	0.500),
+('SUGAR',	2,	0.500),
+('SALT',	1,	0.400),
+('SALT',	2,	0.600);
+
+COMMIT;"""
+    mycursor.execute(sql)
+
+    sql = "INSERT INTO `ingredient` (`name`) VALUES (%s)"
+    val = "" #TODO
+    mycursor.execute(sql, val)
+
+    sql = "INSERT INTO `data_category` (`uuid`, `name`) VALUES (%s, %s)"
+    val = "" #TODO
+    mycursor.execute(sql, val)
+
+    sql = "INSERT INTO `data_product` (`uuid`, `ean`, `category_id`, `name`, `description`, `url_image`, `url_shop`, `price`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    val = "" #todo
+    mycursor.execute(sql, val)
+
+    sql = "INSERT INTO `data_product_ingredient` (`product_id`, `ingredient_name`, `order`, `amount`) VALUES (%s, %s, %s, %s)"
+    val = ""
+    mycursor.execute(sql, val)
 
 sql = "INSERT INTO data_category (name, uuid) VALUES (%s, %s)"
 val = ("Konečně", "to kurva funguje")
